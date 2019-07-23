1d2c470a2e11e1b2a7dd633129e34d5427b87706
Pascal Wild <pascal.wild@mallorca-software.com>
2019-07-23 16:01:14 +0200

    update api client

--- generate.sh
+++ generate.sh
@@ -1,5 +1,9 @@
 openapi-generator generate -g typescript-angular -c config.json -i spec.json
 
+npm install
+npm run build
+tar -cvzf dist.tgz dist
+
 git add --all
 git commit -m "update api client"
 git push
